980c4b8c67bfb79a03ea5a3837cd914338e62846
PandaCarry <nicklaus.park@gmail.com>
2018-03-19 19:32:29 -1000

    added login.html

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import render_template, request
 from app import app 
+from app.forms import LoginForm
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -13,3 +14,8 @@ def index():
 def register():
     user = {'username' : 'nick'}
     return render_template('register.html', title="Sign up here", user=user)
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Sign In", form=form)
