4efb14456eb971e228dd6818dd32f8ee7d4a0a38
PandaCarry <nicklaus.park@gmail.com>
2018-03-19 21:24:32 -1000

    softcoded url routes

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from app import app 
 from app.forms import LoginForm
 
@@ -8,14 +8,23 @@ def index():
     user = {'username' : 'bart'}
     data = request.form
     print('form data', data)
-    return render_template('index.html', title="Welcome to hell", user=user)
+    return render_template('index.html', title="This is a Test Page", user=user)
 
 @app.route('/register')
 def register():
     user = {'username' : 'nick'}
     return render_template('register.html', title="Sign up here", user=user)
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(form.username.data, form.remember_me.data)) 
+        return redirect(url_for('index'))
     return render_template('login.html', title="Sign In", form=form)
+
+
+
+
+
+
